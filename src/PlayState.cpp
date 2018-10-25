d9166ffacf2ba699a57ffddae7a7a50dcf9c7026
Roshan Manjaly <Roshan@Roshan-Manjaly-Macbook-Pro-2.local>
2018-10-25 19:43:18 -0400

    Integration of rocket creation and movement functionality into the GitHub (with rest of the code)
    This was built separately, but prior to GitHub integration, which is the reason for the Co-Authors.
    
    Seth built the skeleton for rocket ship.h file with base movement for one object
    Roshan coded the input to movement functionality added multiple object movement and inertia/momentum (took a lot longer than I thought)
    
    Co-Authored-By: sethbuffkin <sethbuffkin@users.noreply.github.com>

---
+++ src/PlayState.cpp
@@ -0,0 +1,215 @@
+#pragma once
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+#include "rocketShip.h"
+#include "PlayState.h"
+#include "GameState.h"
+#include "GameStateManager.h"
+
+
+PlayState::PlayState(class GameStateManager& gsm) {
+    std::cout << "PlayState declared" << std::endl;
+}
+
+void PlayState::update(float deltams) {
+    std::cout << "PlayState update" << std::endl;
+}
+
+void PlayState::draw(sf::RenderWindow *window) {
+    window->clear(sf::Color::Black);
+    window->draw(ship1.play_ball);
+    window->draw(ship2.play_ball);
+    
+    // display
+    window->display();
+
+    std::cout << "PlayState update" << std::endl;
+}
+
+
+
+void PlayState::handleInput() {
+    
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        ship1.move(135);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A) && sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        ship1.move(225);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        ship1.move(315);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        ship1.move(45);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        ship1.move(90);
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        ship1.move(180);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        ship1.move(270);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        ship1.move(360);
+    }
+    else
+    {
+        ship1.move(0);
+    }
+    
+    
+    
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        ship2.move(135);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        ship2.move(225);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        ship2.move(315);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        ship2.move(45);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+    {
+        ship2.move(90);
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        ship2.move(180);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    {
+        ship2.move(270);
+    }
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        ship2.move(360);
+    }
+    else
+    {
+        ship2.move(0);
+    }
+    
+    std::cout << "PlayState handleInput" << std::endl;
+}
+
+
+//------------------------------------------------------------------------
+
+
+
+
+//class PlayState {
+//public:
+//    rocketShip ship1;
+//    rocketShip ship2;
+//
+//void draw(sf::RenderWindow *window){
+//    window->clear(sf::Color::Black);
+//    window->draw(ship1.play_ball);
+//    window->draw(ship2.play_ball);
+//
+//    // display
+//    window->display();
+//
+//}
+//
+//
+//void handleInput()
+//{
+//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+//    {
+//        ship1.move(135);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::A) && sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+//    {
+//        ship1.move(225);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+//    {
+//        ship1.move(315);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+//    {
+//        ship1.move(45);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+//    {
+//        ship1.move(90);
+//    }
+//    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+//    {
+//        ship1.move(180);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+//    {
+//        ship1.move(270);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+//    {
+//        ship1.move(360);
+//    }
+//    else
+//    {
+//        ship1.move(0);
+//    }
+//
+//
+//
+//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+//    {
+//        ship2.move(135);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+//    {
+//        ship2.move(225);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+//    {
+//        ship2.move(315);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+//    {
+//        ship2.move(45);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+//    {
+//        ship2.move(90);
+//    }
+//    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+//    {
+//        ship2.move(180);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+//    {
+//        ship2.move(270);
+//    }
+//    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+//    {
+//        ship2.move(360);
+//    }
+//    else
+//    {
+//        ship2.move(0);
+//    }
+//
+//
+//
+//}
+//};
