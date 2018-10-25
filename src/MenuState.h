61528f8b19cd0d5c3f5b7fefef0f0e01c4b1ce69
JCOol <liuwukun@d-172-25-98-129.dhcp.virginia.edu>
2018-10-24 21:23:46 -0400

    Initial Push Going to change with respect the change in game loop

--- src/MenuState.h
+++ src/MenuState.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "GameState.h"
-
+#include "<SFML/Graphics.hpp"
 class MenuState : public GameState {
 public:
 	MenuState(class GameStateManager& gsm);
-	void update(float deltams);
-	void draw() {}
-	void handleInput();
+	virtual void update();
+	virtual void draw();
+    virtual void handleInput();
+    
+
+private:
+    void Init(class GameStateManager& gsm);
+    bool MenuState::isClicked(sf::Sprite object, sf::Mouse::Button button, sf::RenderWindow &window);
 };
