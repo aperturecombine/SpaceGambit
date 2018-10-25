1a5965a5a85ea36ec88b1ee8b6625e4c7b10e640
Roshan Manjaly <Roshan@Roshan-Manjaly-Macbook-Pro-2.local>
2018-10-25 18:09:04 -0400

    Removed Johns Code
    Backup has been made

--- src/MenuState.h
+++ src/MenuState.h
@@ -1,15 +1,10 @@
 #pragma once
-#include <SFML/Graphics.hpp>
 #include "GameState.h"
+
 class MenuState : public GameState {
 public:
 	MenuState(class GameStateManager& gsm);
-	virtual void update();
-	virtual void draw();
-    virtual void handleInput();
-    
-
-private:
-    void Init(class GameStateManager& gsm);
-    bool MenuState::isClicked(sf::Sprite object, sf::Mouse::Button button, sf::RenderWindow &window);
+	void update(float deltams);
+	void draw() {}
+	void handleInput();
 };
