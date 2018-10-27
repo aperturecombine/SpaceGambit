ac9ec2683cf02442ea056a34abcbc12079818e9a
SethBuffkin <sethbuffkin@gmail.com>
2018-10-27 16:16:23 -0400

    Changed arguments for building the bullets

--- src/PlayState.h
+++ src/PlayState.h
@@ -14,8 +14,8 @@ public:
     virtual void handleInput();
     rocketShip ship1;
     rocketShip ship2;
-    turret t1 = turret(600, 20);
-    turret t2 = turret(600,500);
+    turret t1 = turret(600, 20, ship1.getPosition(), ship2.getPosition());
+    turret t2 = turret(600,500, ship1.getPosition(), ship2.getPosition());
     
 private:
     GameStateManager *gsm;
