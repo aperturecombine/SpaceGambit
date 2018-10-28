6fb832420662621736526319ea56d07b855be4ea
Roshan Manjaly <Roshan@Roshan-Manjaly-Macbook-Pro-2.local>
2018-10-28 03:53:19 -0400

    Cleaned up MachineGunBullet
    Changed dynamic bullet speed to constant speed. Added option to adjust fire rate.

--- src/machineGunBullet.h
+++ src/machineGunBullet.h
@@ -7,7 +7,8 @@ public:
     sf::CircleShape shell;
     sf::Vector2f direction;
     
-    float speed = 0;
+    float speed = 3;
+    int bulletRate = 500; //used in turret to determine how fast to shoot
     int radius = 10;
     int width = 800;
     int height = 600;
@@ -16,70 +17,71 @@ public:
         
     }
     
-    machineGunBullet(sf::Vector2f direction1, sf::Vector2f direction2, sf::Vector2f turretLocation){
+    machineGunBullet(sf::Vector2f ship1Coord, sf::Vector2f ship2Coord, sf::Vector2f turretLocation){
         
         //create the basic object of the bullet
         shell.setRadius(radius);
         shell.setPointCount(20);
+        shell.setOrigin(radius/2, radius/2);
         
         shell.setPosition(turretLocation.x, turretLocation.y);
         
-        speed = .01;
         
         //compare the distances between the turret and the ships
         //give the bullet the direction of the nearest ship
         //NOTE: if the ships are the same distance, the bullet will fly towrds the one winning the race
-        if( pow(pow(direction1.x - turretLocation.x,2)+pow(direction1.y - turretLocation.y,2),.5) < pow(pow(direction2.x - turretLocation.x,2)+pow(direction2.y - turretLocation.y,2),.5)){
+        if( dist(ship1Coord, turretLocation) < dist(ship2Coord, turretLocation))
+        { // checks if ship1 is closer than ship2
+            //shoots at ship1
+            direction.x = speed*(turretLocation.x - ship1Coord.x)*-1/dist(ship1Coord, turretLocation);
+            direction.y = speed*(turretLocation.y - ship1Coord.y)*-1/dist(ship1Coord, turretLocation);
             
-            direction.x = speed*(turretLocation.x - direction1.x)*-1;
-            direction.y = speed*(turretLocation.y - direction1.y)*-1;
-            
         }
-        else if(pow(pow(direction1.x - turretLocation.x,2)+pow(direction1.y - turretLocation.y,2),.5) > pow(pow(direction2.x - turretLocation.x,2)+pow(direction2.y - turretLocation.y,2),.5)){
-            direction.x = speed*(turretLocation.x - direction2.x)*-1;
-            direction.y = speed*(turretLocation.y - direction2.y)*-1;
+        else if(dist(ship1Coord, turretLocation) > dist(ship2Coord, turretLocation))
+        { // checks if ship2 is closer than ship1
+            //shoots at ship2
+            direction.x = speed*(turretLocation.x - ship2Coord.x)*-1/dist(ship2Coord, turretLocation);
+            direction.y = speed*(turretLocation.y - ship2Coord.y)*-1/dist(ship2Coord, turretLocation);
         }
-        
-        else if(direction1.x > direction2.x){
-            direction.x = speed*(turretLocation.x - direction1.x)*-1;
-            direction.y = speed*(turretLocation.y - direction1.y)*-1;
+        else if(ship1Coord.x > ship2Coord.x)
+        { // here the distances are the same, so its checking if x-coordinate of ship1 is closer than ship2
+            //shoots at ship1
+            direction.x = speed*(turretLocation.x - ship1Coord.x)*-1/dist(ship1Coord, turretLocation);
+            direction.y = speed*(turretLocation.y - ship1Coord.y)*-1/dist(ship1Coord, turretLocation);
         }
-        
-        else {
-            direction.x = speed*(turretLocation.x - direction2.x)*-1;
-            direction.y = speed*(turretLocation.y - direction2.y)*-1;
+        else
+        { // default cause: distances for both ships are the same, and the ship1 x-coordinate is further than ship2 x-coordinate
+            //shoots at ship2
+            direction.x = speed*(turretLocation.x - ship2Coord.x)*-1/dist(ship2Coord, turretLocation);
+            direction.y = speed*(turretLocation.y - ship2Coord.y)*-1/dist(ship2Coord, turretLocation);
         }
         
     }
     
-//    void disposeObject()
-//    {
-////        delete this;
-//    }
+    float dist(sf::Vector2f location1, sf::Vector2f location2)
+    {
+        float distance;
+        distance = pow(pow(location1.x - location2.x,2)+pow(location1.y - location2.y,2),0.5);
+        return distance;
+    }
+    
     
     void move(){
         shell.move(direction.x, direction.y);
-//        std::cout << shell.getPosition().x << std::endl;
-//        std::cout << shell.getPosition().y << std::endl;
-//        if(!withinBounds()){
-//            disposeObject();
-//        }
     }
     
     
     //NOTE: will need to change this later when the screen view is dynamic
     bool withinBounds(){
-        if(shell.getPosition().x + 20 < width &&
-           shell.getPosition().x - 20 > 0 &&
-           shell.getPosition().y + 20 < height &&
-           shell.getPosition().y - 20 > 0)
+        if(shell.getPosition().x + radius/2 < width &&
+           shell.getPosition().x - radius/2 > 0 &&
+           shell.getPosition().y + radius/2 < height &&
+           shell.getPosition().y - radius/2 > 0)
         {
-            std::cout << "Within:" <<shell.getPosition().x<< ", " << shell.getPosition().y << std::endl;
             return true;
         }
         else
         {
-            std::cout << "Outside:" <<shell.getPosition().x<< ", " << shell.getPosition().y << std::endl;
             return false;
         }
     }
