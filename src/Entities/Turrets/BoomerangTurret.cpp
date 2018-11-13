c1ab090e1539b5dc2d11d26730df1639c4fd1510
roshanmanjaly <43384603+roshanmanjaly@users.noreply.github.com>
2018-11-13 18:17:37 -0500

    Changed Skin for Boomerang Turret

--- src/Entities/Turrets/BoomerangTurret.cpp
+++ src/Entities/Turrets/BoomerangTurret.cpp
@@ -7,7 +7,7 @@ BoomerangTurret::BoomerangTurret(sf::Vector2f p) {
     fireRate = .45;
     counter = 0;
     
-    if (!turretImage.loadFromFile("resources/turret.png")) {
+    if (!turretImage.loadFromFile("resources/boomerangTurret.png")) {
         printf("Could not load turret");
     }
     
