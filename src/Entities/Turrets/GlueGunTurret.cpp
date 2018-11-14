265d18a4b2828a74a91c93ca7e5861c85eca28f7
roshanmanjaly <43384603+roshanmanjaly@users.noreply.github.com>
2018-11-14 18:01:32 -0500

    recommiting

--- src/Entities/Turrets/GlueGunTurret.cpp
+++ src/Entities/Turrets/GlueGunTurret.cpp
@@ -26,12 +26,11 @@ GlueGunTurret::GlueGunTurret(sf::Vector2f p) {
 }
 
 void GlueGunTurret::fire() {
-    sf::Vector2f v = getInitBulletVel();
-    if (withinfiringRange)
-    {
-//    GlueGunBullet *newBullet = new GlueGunBullet(pos, getInitBulletVel());
-//    bullets.push_back(newBullet);
-    }
+//    if (withinfiringRange)
+//    {
+////    GlueGunBullet *newBullet = new GlueGunBullet(pos, getInitBulletVel());
+////    bullets.push_back(newBullet);
+//    }
 }
 
 void GlueGunTurret::update(float dt) {
@@ -88,24 +87,24 @@ void GlueGunTurret::update(float dt) {
 }
 
 sf::Vector2f GlueGunTurret::getInitBulletVel() {
-//    sf::Vector2f ship1_init = (ref->ship1.pos - pos);
-//    sf::Vector2f ship2_init = (ref->ship2.pos - pos);
-//    float ship1_dist = pow((ship1_init.x*ship1_init.x + ship1_init.y*ship1_init.y),0.5);
-//    float ship2_dist = pow((ship2_init.x*ship2_init.x + ship2_init.y*ship2_init.y),0.5);
-//
-//
-//    if (ship1_dist < ship2_dist)
-//    {
-//        if (ship1_dist < firingRange) {withinfiringRange=true;}
-//        else {withinfiringRange=false;}
-//        return normalize(ship1_init);
-//    }
-//    else
-//    {
-//        if (ship1_dist < firingRange) {withinfiringRange=true;}
-//        else {withinfiringRange=false;}
-//        return normalize(ship2_init);
-//    }
+    sf::Vector2f ship1_init = (ref->ship1.pos - pos);
+    sf::Vector2f ship2_init = (ref->ship2.pos - pos);
+    float ship1_dist = pow((ship1_init.x*ship1_init.x + ship1_init.y*ship1_init.y),0.5);
+    float ship2_dist = pow((ship2_init.x*ship2_init.x + ship2_init.y*ship2_init.y),0.5);
+
+
+    if (ship1_dist < ship2_dist)
+    {
+        if (ship1_dist < firingRange) {withinfiringRange=true;}
+        else {withinfiringRange=false;}
+        return normalize(ship1_init);
+    }
+    else
+    {
+        if (ship1_dist < firingRange) {withinfiringRange=true;}
+        else {withinfiringRange=false;}
+        return normalize(ship2_init);
+    }
 }
 
 sf::Vector2f GlueGunTurret::normalize(sf::Vector2f & v) {
@@ -115,32 +114,3 @@ sf::Vector2f GlueGunTurret::normalize(sf::Vector2f & v) {
     else
         return v;
 }
-
-/*void setDirection(int angle) {
- switch (angle) {
- case 90: // Moving straight up
- direction.x = 0;
- direction.y = -windowHeight / 20;
- break;
- 
- case 180: // Moving straight left
- direction.x = -windowWidth / 20;
- direction.y = 0;
- break;
- 
- case 270: // Moving straight down
- direction.x = 0;
- direction.y = windowHeight / 20;
- break;
- 
- case 360: // Moving straight right
- direction.x = windowWidth / 20;
- direction.y = 0;
- break;
- 
- default: // There should be no direction
- direction.x = 0;
- direction.y = 0;
- break;
- }
- }*/
