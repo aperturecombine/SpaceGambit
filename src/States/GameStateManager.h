ba4419707628a815f48bed3cceb12bf36ff43ea8
roshanmanjaly <43384603+roshanmanjaly@users.noreply.github.com>
2018-11-14 17:30:08 -0500

    added includes to GameStateManager

--- src/States/GameStateManager.h
+++ src/States/GameStateManager.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <stack>
+#include "GameState.h"
 #include "MenuState.h"
 #include "PlayState.h"
+#include "FinishState.h"
+#include "OptionState.h"
 #include "../Entities/Ships/RocketShip.h"
 #include <SFML/Graphics.hpp>
 
 #define MENUSTATE 0
 #define PLAYSTATE 1
 #define FINISHSTATE 2
+#define OPTIONSTATE 3
 
 class GameStateManager {
 public:
