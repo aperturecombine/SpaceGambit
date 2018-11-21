8050f399cd4ae7bff83573c118ae249f13787810
Matthew Cohen <matthew_cohen97@me.com>
2018-11-21 18:36:52 -0500

    changed main.cpp headers

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
-#include "./States/GameStateManager.h"
-#include "Box2D/Box2D.h"
+#include "../include/States/GameStateManager.h"
+#include <Box2D/Box2D.h>
 
 int main() {
 	GameStateManager gsm;
